568c6ea787f2a90f1c7d7f0fbebb8e776a031fc2
IqbalSalim <iqbalbinsalim@gmail.com>
2024-04-05 01:06:57 +0800

    handle upload image in create publikasi

--- resources/js/Pages/Publikasi/Create.jsx
+++ resources/js/Pages/Publikasi/Create.jsx
@@ -5,7 +5,7 @@ import TextArea from "@/Components/TextArea";
 import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, useForm } from "@inertiajs/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
 import Editor from "react-simple-wysiwyg";
@@ -17,6 +17,7 @@ export default function Create({ auth, kategoris }) {
     tempat: "",
     tanggal_kegiatan: "",
     status: "",
+    gambar: [],
     kategori: [],
   });
 
@@ -35,12 +36,68 @@ export default function Create({ auth, kategoris }) {
     setData("kategori", selectedOptions);
   };
 
+  const [images, setImages] = useState([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const fileInputRef = useRef(null);
+
+  function selectFiles() {
+    fileInputRef.current.click();
+  }
+
+  function onFileSelect(event) {
+    event.preventDefault();
+    const files = event.target.files;
+    if (files.length === 0) return;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type.split("/")[0] !== "image") continue;
+      if (!images.some((e) => e.name === files[i].name)) {
+        setImages((prevImages) => [
+          ...prevImages,
+          {
+            name: files[i].name,
+            url: URL.createObjectURL(files[i]),
+          },
+        ]);
+      }
+    }
+  }
+  function deleteImage(index) {
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+    setIsDragging(true);
+    event.dataTransfer.dropEffect = "copy";
+  }
+  function onDragLeave(event) {
+    event.preventDefault();
+    setIsDragging(false);
+  }
+  function onDrop(event) {
+    event.preventDefault();
+    setIsDragging(false);
+    const files = event.dataTransfer.files;
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type.split("/")[0] !== "image") continue;
+      if (!images.some((e) => e.name === files[i].name)) {
+        setImages((prevImages) => [
+          ...prevImages,
+          {
+            name: files[i].name,
+            url: URL.createObjectURL(files[i]),
+          },
+        ]);
+      }
+    }
+  }
+
   return (
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <div className="flex justify-between items-center">
-          <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             Tambah Publikasi Baru
           </h2>
         </div>
@@ -49,12 +106,11 @@ export default function Create({ auth, kategoris }) {
       <Head title="Publikasi" />
 
       <div className="py-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          {/* <pre>{JSON.stringify(kategoris, undefined, 2)}</pre> */}
-          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
             <form
               onSubmit={onSubmit}
-              className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
+              className="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg"
             >
               <div className="mt-4">
                 <InputLabel htmlFor="judul" value="Judul" />
@@ -64,7 +120,7 @@ export default function Create({ auth, kategoris }) {
                   type="text"
                   name="name"
                   value={data.judul}
-                  className="mt-1 block w-full"
+                  className="block w-full mt-1"
                   isFocused={true}
                   onChange={(e) => setData("judul", e.target.value)}
                 />
@@ -72,7 +128,7 @@ export default function Create({ auth, kategoris }) {
                 <InputError message={errors.judul} className="mt-2" />
               </div>
 
-              <div className="grid gap-4 grid-cols-2">
+              <div className="grid grid-cols-2 gap-4">
                 <div className="mt-4">
                   <InputLabel htmlFor="tempat" value="Tempat" />
 
@@ -131,6 +187,72 @@ export default function Create({ auth, kategoris }) {
                 <InputError message={errors.kategori} className="mt-2" />
               </div>
               <div className="mt-4">
+                <InputLabel htmlFor="gambar" value="Upload Gambar" />
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                  <label
+                    onDragOver={onDragOver}
+                    onDragLeave={onDragLeave}
+                    onDrop={onDrop}
+                    htmlFor="gambar"
+                    className="relative flex items-center justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer select-none hover:opacity-50 hover:border-gray-400 focus:outline-none"
+                  >
+                    <span className="flex items-center space-x-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6 text-gray-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                        />
+                      </svg>
+                      <span
+                        className="font-medium text-gray-600"
+                        role="button"
+                        onClick={selectFiles}
+                      >
+                        Drop files to Attach, or
+                        <span className="text-blue-600 underline">browse</span>
+                      </span>
+                    </span>
+                  </label>
+                  <TextInput
+                    id="gambar"
+                    name="gambar"
+                    className="absolute hidden w-full h-full"
+                    required
+                    type="file"
+                    multiple
+                    ref={fileInputRef}
+                    onChange={onFileSelect}
+                  />
+                  <div className="grid grid-cols-4 gap-2 p-2 bg-white border-2 border-gray-300 rounded-md md:grid-cols-6 md:gap-4">
+                    {images.map((images, index) => (
+                      <div key={index} className="relative w-16 h-16 ">
+                        <span
+                          onClick={() => deleteImage(index)}
+                          className="absolute p-1 text-blue-700 bg-white rounded-md cursor-pointer hover:opacity-50 delete -top-1 right-2"
+                        >
+                          &times;
+                        </span>
+                        <img
+                          className="object-fill w-16 h-16 rounded-md"
+                          src={images.url}
+                          alt={images.name}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+
+                <InputError message={errors.tempat} className="mt-2" />
+              </div>
+              <div className="mt-4">
                 <InputLabel htmlFor="isi" value="Deskripsi" />
                 <Editor
                   id="isi"
@@ -145,19 +267,19 @@ export default function Create({ auth, kategoris }) {
               <div className="mt-4 text-right">
                 <Link
                   href={route("publikasi.index")}
-                  className="bg-gray-100 py-1 px-3 text-gray-800 rounded shadow transition-all hover:bg-gray-200 mr-2"
+                  className="px-3 py-1 mr-2 text-gray-800 transition-all bg-gray-100 rounded shadow hover:bg-gray-200"
                 >
                   Cancel
                 </Link>
                 <button
                   onClick={() => setData("status", "draft")}
-                  className="bg-purple-500 py-1 px-3 mr-2 text-white rounded shadow transition-all hover:bg-emerald-600"
+                  className="px-3 py-1 mr-2 text-white transition-all bg-purple-500 rounded shadow hover:bg-emerald-600"
                 >
                   Save For Draft
                 </button>
                 <button
                   onClick={() => setData("status", "published")}
-                  className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+                  className="px-3 py-1 text-white transition-all rounded shadow bg-emerald-500 hover:bg-emerald-600"
                 >
                   Submit For Publish
                 </button>
